8459d2ffb3a721ec5004aa8afc9ffa62ff2d99b8
DarkOwl999 <minhc2e2@gmail.com>
2026-03-26 21:52:08 +0700

    "thêm model apartment và migration đầu tiên by Hiếu"

--- FW_Xay_dung_Website_cho_thue_homestay/myapp/models.py
+++ FW_Xay_dung_Website_cho_thue_homestay/myapp/models.py
@@ -1,2 +1,20 @@
 from django.db import models
 
+
+class Apartment(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Tên căn hộ")
+    price = models.CharField(max_length=50, verbose_name="Giá tiền (VNĐ)")
+    address = models.CharField(max_length=255, verbose_name="Địa chỉ")
+    desc = models.TextField(verbose_name="Mô tả", blank=True)
+    image = models.ImageField(upload_to='apartments/', verbose_name="Hình ảnh", null=True, blank=True)
+    lat = models.FloatField(verbose_name="Vĩ độ (Latitude)")
+    lng = models.FloatField(verbose_name="Kinh độ (Longitude)")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Căn hộ"
+        verbose_name_plural = "Danh sách Căn hộ"
+
+
