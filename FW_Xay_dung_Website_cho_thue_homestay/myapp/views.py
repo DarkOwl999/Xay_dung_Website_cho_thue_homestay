4a7e905a410b9a5435a08477f6a6bde54caf2c7e
DarkOwl999 <minhc2e2@gmail.com>
2026-03-26 22:18:21 +0700

    "Thêm WebMap cơ bản bằng Leaflet và trang chi tiết từng căn hộ by Giang"

--- FW_Xay_dung_Website_cho_thue_homestay/myapp/views.py
+++ FW_Xay_dung_Website_cho_thue_homestay/myapp/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Apartment
 
 
 def home(request):
-    return render(request, 'home.html')
+    latest_apartments = Apartment.objects.all().order_by('-id')[:3]
+    return render(request, 'home.html', {'latest_apartments': latest_apartments})
+
+
+def map_view(request):
+    apartments_db = Apartment.objects.all()
+    apartments_list = []
+    for apt in apartments_db:
+        apartments_list.append({
+            'id': apt.id,
+            'name': apt.name,
+            'price': apt.price,
+            'address': apt.address,
+            'desc': apt.desc,
+            'image': apt.image.url if apt.image else '',
+            'lat': apt.lat,
+            'lng': apt.lng,
+        })
+    return render(request, 'map.html', {'apartments': apartments_list})
+
+
+def apartment_detail(request, apartment_id):
+    apartment = get_object_or_404(Apartment, pk=apartment_id)
+    return render(request, 'apartment_detail.html', {'apartment': apartment})
